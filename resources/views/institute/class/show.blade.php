e046974da97c81672c8d8ad3865ac9a6fdc3f66a
YYusra <grands.yusra@gmail.com>
2024-01-29 11:42:09 +0500

    dashboard

--- resources/views/institute/class/show.blade.php
+++ resources/views/institute/class/show.blade.php
@@ -15,7 +15,7 @@
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                                <li class="breadcrumb-item active">institute</li>
+                                <li class="breadcrumb-item active">Institute</li>
                             </ol>
                         </div>
                         <h4 class="page-title">class details</h4>
